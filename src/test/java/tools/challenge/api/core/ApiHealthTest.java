9c52e813393940a4061d2096947dca30b895d6d7
Fredson Chaves <43495376+fredsonchaves07@users.noreply.github.com>
2025-11-19 10:17:14 -0300

    Estrutura inicial da API (#36)
    * feat: Estrutura inicial da api
    
    * feat: Estrutura inicial de health check
    
    * feat: Adicionando configurações de health check
    
    * build: Configurações de variaveis e propriedades de ambiente
    
    * build: Configurações de conexão de banco de dados
    
    * build: Containerização da aplicação
    
    * fix: Ajuste em propriedades de testes
    
    * fix: Ajuste em workflow com as tags corretas do docker
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ src/test/java/tools/challenge/api/core/ApiHealthTest.java
@@ -0,0 +1,37 @@
+package tools.challenge.api.core;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.response.Response;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static tools.challenge.api.core.ApiHealth.HEALTH_VERSION_UNKNOWN;
+
+@QuarkusTest
+public class ApiHealthTest {
+
+    @ConfigProperty(name = "quarkus.application.version", defaultValue = HEALTH_VERSION_UNKNOWN)
+    private String version;
+
+    private Response responseHealthCheck() {
+        return given()
+                .when()
+                .get("/")
+                .then()
+                .extract()
+                .response();
+    }
+
+    @Test
+    public void deveSerPossivelConsultarHealthCheckDaApi() {
+        Response response = responseHealthCheck();
+        assertEquals(200, response.statusCode());
+        assertEquals("Running", responseHealthCheck().jsonPath().getString("status"));
+        assertEquals(version, responseHealthCheck().jsonPath().getString("version"));
+        assertEquals(LocalDate.now().toString(), responseHealthCheck().jsonPath().getString("buildDate"));
+    }
+}
