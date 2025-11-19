5282864d53b5a0046fa29fc9bc7809a534c9a683
Fredson Chaves <fredsonchaves07@gmail.com>
2025-11-18 22:07:54 -0300

    feat: Estrutura inicial da api

---
+++ src/test/java/tools/challenge/api/core/ApiHealthTest.java
@@ -0,0 +1,14 @@
+//package tools.challenge.api.core;
+//
+//@QuarkusTest
+//public class ApiHealthTest {
+//
+//    private Response responseHealthCheck() {
+//        return given()
+//                .when()
+//                .get("/")
+//                .then()
+//                .extract()
+//                .response();
+//    }
+//}
