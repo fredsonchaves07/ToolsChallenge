8aa44414bf8522960dfcfbb38248cea6f76feafd
Fredson Chaves <fredsonchaves07@gmail.com>
2025-11-19 08:00:27 -0300

    feat: Estrutura inicial de health check

--- src/test/java/tools/challenge/api/core/ApiHealthTest.java
+++ src/test/java/tools/challenge/api/core/ApiHealthTest.java
@@ -1,14 +1,32 @@
-//package tools.challenge.api.core;
-//
-//@QuarkusTest
-//public class ApiHealthTest {
-//
-//    private Response responseHealthCheck() {
-//        return given()
-//                .when()
-//                .get("/")
-//                .then()
-//                .extract()
-//                .response();
-//    }
-//}
+package tools.challenge.api.core;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@QuarkusTest
+public class ApiHealthTest {
+
+    private Response responseHealthCheck() {
+        return given()
+                .when()
+                .get("/")
+                .then()
+                .extract()
+                .response();
+    }
+
+    @Test
+    public void deveSerPossivelConsultarHealthCheckDaApi() {
+        Response response = responseHealthCheck();
+        assertEquals(200, response.statusCode());
+        assertEquals("Running", responseHealthCheck().jsonPath().getString("status"));
+        assertEquals("test", responseHealthCheck().jsonPath().getString("version"));
+        assertEquals(LocalDate.now().toString(), responseHealthCheck().jsonPath().getString("buildDate"));
+    }
+}
